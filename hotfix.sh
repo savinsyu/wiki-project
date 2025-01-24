3c8c69b06b937637de32ba743aee3a80599194f7
savinsyu <137873798+savinsyu@users.noreply.github.com>
2025-01-24 11:06:25 +0300

    Commit: Fri Jan 24 11:06:20 RTZ 2025

--- hotfix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DATE=$(date)
-TEXT="Hotfix: $DATE"
-git add .
-git commit -m "$TEXT"
-git push
-time=$(date +%s)
-echo $(($(date +%s)-$time))
--- modules/dump_full.py
+++ modules/dump_full.py
@@ -1,16 +1,26 @@
-import connect
-import datetime
 import sqlite3
+import io
+import os.path
 
-
-def dump_full():
-    today = datetime.datetime.today()
-    conn = connect.get_db_connection()
+path_to_check = "../database.db"
+if os.path.exists(path_to_check):
+    print("Файл базы данных существует")
+    conn = sqlite3.connect('../database.db')
     b_conn = sqlite3.connect('../backup.db')
     conn.backup(b_conn)
     b_conn.close()
+    print('Полный дамп базы данных готов!!')
     conn.close()
-    print('Полный дамп от', (today.strftime("%Y-%m-%d %H.%M.%S")), ' готов!!')
+    conn = sqlite3.connect("../database.db")
+    conn.row_factory = sqlite3.Row
+    # RUS Сохраняем в файл дамп базы данных (папка dump)
+    # ENG Save the database dump to a file (dump folder)
+    with io.open("../database_dump.sql", 'w') as p:
+        # iterdump() function
+        for line in conn.iterdump():
+            p.write('%s\n' % line)
 
-
-dump_full()
+    print('Дамп базы данных готов!!')
+    conn.close()
+else:
+    print("Файл базы данных не существует.")
