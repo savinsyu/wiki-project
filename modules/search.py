edb2ef62e62739c6c4186452ea75206c339a2911
savinsyu <137873798+savinsyu@users.noreply.github.com>
2025-02-06 16:04:53 +0300

    Rus. Реализован поиск по всем таблицам баз данных. Eng. Implemented search across all database tables.

--- modules/search.py
+++ modules/search.py
@@ -12,8 +12,22 @@ def search():
         search_term = request.form['search_term']
         conn = connect.get_db_connection()
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM python WHERE python_command LIKE ? OR python_name LIKE ?',
-                       (f'%{search_term}%', f'%{search_term}%'))
+        query = '''
+        SELECT * FROM python 
+        WHERE python_command LIKE ? OR python_name LIKE ? 
+        UNION 
+        SELECT * FROM cli 
+        WHERE cli_command LIKE ? OR cli_name LIKE ?
+        UNION
+        SELECT * FROM sql 
+        WHERE sql_command LIKE ? OR sql_name LIKE ?
+        '''
+        cursor.execute(query,(f'%{search_term}%', 
+                              f'%{search_term}%', 
+                              f'%{search_term}%', 
+                              f'%{search_term}%', 
+                              f'%{search_term}%', 
+                              f'%{search_term}%'))
         results = cursor.fetchall()
         return render_template('search.html', results=results)
     return render_template('search.html')
