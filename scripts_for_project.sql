9a7a4d51dd416bd888ce89517949d1743df77703
savinsyu <137873798+savinsyu@users.noreply.github.com>
2025-02-14 15:08:25 +0300

    Rus. Переделана верстка всех разделов приложения. Eng. The layout of all sections of the application has been redesigned.

--- scripts_for_project.sql
+++ scripts_for_project.sql
@@ -1,3 +1,5 @@
-UPDATE about SET about_name ='В разделе О проекте реализована логика добавления и удаления записей, создана таблица в базе данных соответсвующая;' WHERE about_id = 12;
+UPDATE about SET about_name ='В разделе «О проекте» реализована логика добавления и удаления записей, создана таблица в базе данных, соответствующая;' WHERE about_id = 12;
 SELECT * FROM about WHERE about.about_id = 12;
 SELECT * FROM about WHERE about_name LIKE '%тест%';
+SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%';
+PRAGMA table_info(python);
