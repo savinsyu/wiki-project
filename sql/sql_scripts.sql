9b390df50e5897e77c99bcad669428568719c4f1
savinsyu <137873798+savinsyu@users.noreply.github.com>
2026-09-23 16:39:23 +0300

    Hotfix

--- sql/sql_scripts.sql
+++ sql/sql_scripts.sql
@@ -1,3 +1,28 @@
 CREATE TABLE IF NOT EXISTS games (game_id INTEGER PRIMARY KEY AUTOINCREMENT,
  game_name TEXT NOT NULL,
  game_genre TEXT NOT NULL);
+ 
+INSERT INTO games (game_name, game_genre) VALUES ('Warcraft','RPG');
+INSERT INTO games (game_name, game_genre) VALUES ('StarCraft','RTS');
+INSERT INTO games (game_name, game_genre) VALUES ('Battlefield','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Disco Elysium','RPG');
+INSERT INTO games (game_name, game_genre) VALUES ('Borderlands','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('GTA Vice City','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('GTA San Andreas','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('GTA 3','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Age of Empires','RTS');
+INSERT INTO games (game_name, game_genre) VALUES ('Starfield','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Titanfall','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Deus Ex','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('BioShock infinite','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Half-life','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Doom','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Quake','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Metal Gear Solid','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Mass Effect','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Max Payne','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Mortal Kombat','Fighting');
+INSERT INTO games (game_name, game_genre) VALUES ('Tekken','Fighting');
+INSERT INTO games (game_name, game_genre) VALUES ('Street Fighter','Fighting');
+INSERT INTO games (game_name, game_genre) VALUES ('Nier Autamata','Action');
+INSERT INTO games (game_name, game_genre) VALUES ('Stellar Blade','Action');
